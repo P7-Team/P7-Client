2db65d9f5f9349f84b7603f9a6fd14cbc445672a
Mikael <mimikk21@student.aau.dk>
2021-11-17 11:37:37 +0100

    HeartbeatClient done?
    Should be done and work but still have to implement tests.

---
+++ Client-app unit tests/HeartbeatClientTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+
+namespace Client_app
+{
+    public class HeartbeatClientTests
+    {
+        //TODO: Implement test of heartbeats
+
+        public HeartbeatClientTests()
+        {
+            
+        }
+
+        //TODO - TEST: Recieve OK from 
+        //TODO - TEST: Received Error for sending nonsense
+
+        [Fact]
+        public void TestsIsRun()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
---
+++ Client/Clients/HeartbeatClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text.Json.Serialization;
+using Client.Interfaces;
+using Client.Models;
+using Newtonsoft.Json;
+
+namespace Client.Clients
+{
+     
+    public class HeartbeatClient
+    {
+        //TODO: Make methods so that the client is able to send heartbeats, telling the scheduler of our status. Send the info as JSON file.
+        // Currently types of heartbeats are Working, Done, ShuttingDown.
+        // Working: I'm working on a task. Done: I'm done working on a task, please acknowledge. Shuttingdown: I'm being shut down, reschedule my task.
+        
+        private IHttpClient _client;
+        public HeartbeatClient(IHttpClient client)
+        {
+            _client = client;
+        }
+        
+    }
+}
