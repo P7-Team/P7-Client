3a76e4240ab6b67e6cd4c53e089104f09cf804b1
Mikael <mimikk21@student.aau.dk>
2021-11-17 15:05:22 +0100

    Squash Target 0

--- Client-app unit tests/HeartbeatClientTests.cs
+++ Client-app unit tests/HeartbeatClientTests.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using Client.Clients;
+using Client.Interfaces;
+using Client.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
 using Xunit;
 
 
@@ -6,15 +17,29 @@ namespace Client_app
 {
     public class HeartbeatClientTests
     {
-        //TODO: Implement test of heartbeats
-
+        
         public HeartbeatClientTests()
         {
             
         }
 
-        //TODO - TEST: Recieve OK from 
+        //TODO - TEST: Recieve OK from method Working, Done, ShuttingDown
         //TODO - TEST: Received Error for sending nonsense
+
+        [Fact]
+        public void TestRecieveOK()
+        {
+
+            
+            
+        }
+
+        [Fact]
+        public void TestStatusInvalid()
+        {
+            var mockHandler = new Mock<HttpMessageHandler>();
+
+        }
 
         [Fact]
         public void TestsIsRun()
--- Client/Clients/HeartbeatClient.cs
+++ Client/Clients/HeartbeatClient.cs
@@ -6,6 +6,8 @@ using System.Text.Json.Serialization;
 using Client.Interfaces;
 using Client.Models;
 using Newtonsoft.Json;
+using System.Text;
+
 
 namespace Client.Clients
 {
@@ -15,12 +17,48 @@ namespace Client.Clients
         //TODO: Make methods so that the client is able to send heartbeats, telling the scheduler of our status. Send the info as JSON file.
         // Currently types of heartbeats are Working, Done, ShuttingDown.
         // Working: I'm working on a task. Done: I'm done working on a task, please acknowledge. Shuttingdown: I'm being shut down, reschedule my task.
-        
-        private IHttpClient _client;
-        public HeartbeatClient(IHttpClient client)
+
+
+        private IHttpService _client;
+        public HeartbeatClient(IHttpService client)
         {
             _client = client;
         }
-        
+
+        private bool SendHeartbeat(string messageType)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Post, "api/HeartBeat");
+            
+            message.Content = new StringContent(
+                "{\"MessageType\":\"#type#\"}".Replace("#type#", messageType),
+                Encoding.UTF8,
+                "application/json"); //Type of Content
+
+            HttpResponseMessage respone = _client.Send(message);
+
+            if (respone.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SendHeartbeatWorking()
+        {
+            return SendHeartbeat("Working");
+        }
+
+        public bool SendHeartbeatDone()
+        {
+            return SendHeartbeat("Done");
+        }
+
+        public bool SendHeartbeatShuttingDown()
+        {
+            return SendHeartbeat("ShuttingDown");
+        }
     }
 }
