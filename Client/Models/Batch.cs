882e770ef73c9854b45c6668c0b918340d5feaa3
Alex <alex.klitgaard@gmail.com>
2021-11-18 12:02:54 +0100

    Created a Batch model

---
+++ Client/Models/Batch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Client.Models
+{
+    public class Batch
+    {
+        public string Id { get; set; }
+        public Stream Executable { get; set; }
+        public List<Stream> Inputs { get; set; }
+
+        public Batch(string id, Stream executable, List<Stream> inputs)
+        {
+            Id = id;
+            Executable = executable;
+            Inputs = inputs;
+        }
+    }
+}
