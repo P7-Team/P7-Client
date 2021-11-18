d13d6b8e976af09609eae08675ee15fe16ff963d
Alex <alex.klitgaard@gmail.com>
2021-11-18 12:03:42 +0100

    Started implementing BatchClientTests

---
+++ Client-app unit tests/BatchClientTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using Client.Interfaces;
+
+namespace Client_app
+{
+    public class BatchClientTest
+    {
+        public class SendBatchClient : IHttpService
+        {
+            public HttpContent Content { get; private set; }
+            
+            public HttpResponseMessage Send(HttpRequestMessage message)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public HttpResponseMessage Get(string uri)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public HttpResponseMessage Post(string uri, HttpContent content)
+            {
+                Content = content;
+                return new HttpResponseMessage();
+            }
+
+            public HttpResponseMessage Delete(string uri)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
