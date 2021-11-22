7b3d349dc60e01f355a00f267ef108a62bb05102
Mikael Vind Mikkelsen <90759827+MikaelVM@users.noreply.github.com>
2021-11-22 11:32:53 +0100

    Update Client/Clients/HeartbeatClient.cs
    Co-authored-by: Christian Hartvigsen  <38457971+zaztz@users.noreply.github.com>

--- Client/Clients/HeartbeatClient.cs
+++ Client/Clients/HeartbeatClient.cs
@@ -9,9 +9,6 @@ namespace Client.Clients
      
     public class HeartbeatClient
     {
-        //TODO: Make methods so that the client is able to send heartbeats, telling the scheduler of our status. Send the info as JSON file.
-        // Currently types of heartbeats are Working, Done, ShuttingDown.
-        // Working: I'm working on a task. Done: I'm done working on a task, please acknowledge. Shuttingdown: I'm being shut down, reschedule my task.
 
 
         private IHttpService _client;
