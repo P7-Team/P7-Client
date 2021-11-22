d97a608ea6433c10998aeaf0a1662596cc3b8407
Alexander N <alax0013@gmail.com>
2021-11-22 11:06:38 +0100

    Intermediate commit.

--- Client/Interfaces/IUserClient.cs
+++ Client/Interfaces/IUserClient.cs
@@ -2,9 +2,7 @@ namespace Client.Interfaces
 {
     public interface IUserClient
     {
-        public string Token { get; }
-
-        public bool Login(string username, string password);
+        public string LoginUser(string username, string password);
 
         public bool CreateUser(string username, string password);
     }
--- Client/Services/ClientStateManager.cs
+++ Client/Services/ClientStateManager.cs
@@ -1,13 +1,32 @@
+using System;
+using System.Threading.Tasks;
 using Client.Interfaces;
 
 namespace Client.Services
 {
     // TODO Make Async
+    // TODO SetUP basic UI.
     public class ClientStateManager
     {
-        public ClientStateManager(IHttpService httpService,ITaskClient taskClient, IInterpretedTaskCompleter interpretedTaskCompleter)
+        private string _token = "";
+        private IHttpService _httpService;
+        private ITaskClient _taskClient;
+        private IInterpretedTaskCompleter _interpretedTaskCompleter;
+        private IHeartbeatController _heartbeatController;
+
+        public ClientStateManager(IHttpService httpService, ITaskClient taskClient,
+            IInterpretedTaskCompleter interpretedTaskCompleter, IHeartbeatController heartbeatController)
         {
-            
+            _httpService = httpService;
+            _taskClient = taskClient;
+            _interpretedTaskCompleter = interpretedTaskCompleter;
+            _heartbeatController = heartbeatController;
+        }
+
+        public async Task Run(string token)
+        {
+            _token = token;
+            _taskClient.GetTask();
         }
     }
 }
