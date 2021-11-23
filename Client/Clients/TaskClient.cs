b3d7842aab1b444b0bc7b11d4bba00ea4427c1a8
Alexander N <alax0013@gmail.com>
2021-11-23 09:54:52 +0100

    Updated TaskClient.cs with comments + fixed todo for refactoring.

--- Client/Clients/TaskClient.cs
+++ Client/Clients/TaskClient.cs
@@ -12,18 +12,25 @@ namespace Client.Clients
     public class TaskClient : ITaskClient
     {
         private IHttpService _service;
+
         public TaskClient(IHttpService service)
         {
             _service = service;
         }
 
+        /// <summary>
+        /// Gets a task from the webservice.
+        /// </summary>
+        /// <returns>Returns the task if one is available, returns null if there is none available.</returns>
         public Task GetTask()
         {
             HttpResponseMessage response = _service.Get("/api/task/ready");
 
             if (response.IsSuccessStatusCode)
             {
-                var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content.ReadAsStringAsync().Result);
+                var values =
+                    JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content.ReadAsStringAsync()
+                        .Result);
 
                 var task = new Task(values["source"], values["input"]);
 
@@ -32,23 +39,26 @@ namespace Client.Clients
 
             return null;
         }
-        
+
+        /// <summary>
+        /// Converts a completed task to multipartFormDataContent. And posts it to the webservice
+        /// </summary>
+        /// <param name="completedTask"></param>
+        /// <returns>Returns the status of the request.</returns>
         public bool AddResult(CompletedTask completedTask)
         {
             Dictionary<string, string> formdata = new Dictionary<string, string>()
             {
-                {"id", completedTask.Id.ToString() }
+                {"id", completedTask.Id.ToString()}
             };
             Dictionary<string, Stream> files = new Dictionary<string, Stream>()
             {
-                { "result", completedTask.FileStream }
+                {"result", completedTask.FileStream}
             };
 
             MultipartFormDataContent content = MultipartFormDataHelper.CreateContent(formdata, files);
-            // TODO: Refactor to use the new Post after merging
-            HttpResponseMessage res = _service.Send(new HttpRequestMessage() { Content = content });
 
-            return res.IsSuccessStatusCode;
+            return _service.Post("/task/complete", content).IsSuccessStatusCode;
         }
     }
 }
