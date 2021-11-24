ae856d3a2bae8734a68ca70434e1ec5f260fad5a
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2021-11-24 09:51:02 +0100

    Added some public's to keep consistent

--- Client/Interfaces/IBatchClient.cs
+++ Client/Interfaces/IBatchClient.cs
@@ -5,7 +5,7 @@ namespace Client.Interfaces
 {
     public interface IBatchClient
     {
-        bool AddBatch(Batch batch);
+        public bool AddBatch(Batch batch);
         
         public IEnumerable<Batch> GetBatchStatus();
 
--- Client/Interfaces/IHttpService.cs
+++ Client/Interfaces/IHttpService.cs
@@ -4,11 +4,11 @@ namespace Client.Interfaces
 {
     public interface IHttpService
     {
-        HttpResponseMessage Send(HttpRequestMessage message);
-        HttpResponseMessage Get(string uri);
-        HttpResponseMessage Post(string uri, HttpContent content);
-        HttpResponseMessage Delete(string uri);
+        public HttpResponseMessage Send(HttpRequestMessage message);
+        public HttpResponseMessage Get(string uri);
+        public HttpResponseMessage Post(string uri, HttpContent content);
+        public HttpResponseMessage Delete(string uri);
 
-        void SetToken(string token);
+        public void SetToken(string token);
     }
 }
