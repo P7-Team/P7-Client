ef4c7690bbc7c5676da03cf4f0fbeb6751d6bd5c
Alexander Nykjær <alax0013@gmail.com>
2021-11-24 09:41:43 +0100

    Update Client/Services/ClientStateManager.cs
    Co-authored-by: Christian Hartvigsen  <38457971+zaztz@users.noreply.github.com>

--- Client/Services/ClientStateManager.cs
+++ Client/Services/ClientStateManager.cs
@@ -78,7 +78,7 @@ namespace Client.Services
                 return;
             }
 
-            List<Batch> batches = (List<Batch>) _batchInterface.GetBatchStatus();
+            List<BatchStatus > batches = (List<BatchStatus >) _batchInterface.GetBatchStatus();
             // TODO handle downloaded batches.
             if (batches.Count > 0)
             {
